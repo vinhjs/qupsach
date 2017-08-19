13bca0108d15143cc572f124db7052bf1f488188
vinhjs <vinhjs@gmail.com>
2017-08-20 00:31:32 +0700

    init

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+  <a href="http://chatbot.vn">ChatbotVN</a>,
+  rootEl
+)
+
+render()
